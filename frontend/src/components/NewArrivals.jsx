bcd808167e62501a89faa16d2534725fef440ce4
Pham Trung Nam <phamtrungnam279@gmail.com>
2025-05-19 23:25:02 +0700

    hoan thien frontend

--- frontend/src/components/NewArrivals.jsx
+++ frontend/src/components/NewArrivals.jsx
@@ -15,7 +15,7 @@ const NewArrivals = () => {
   return (
     <section className='max-padd-container py-16'>
       <Title title={'New Arrivals'} titleStyles={'text-center'}/>
-      <div className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-8'>
+      <div className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
         {newArrivals.map((product) => (
           <div key={product._id}>
             <Item product={product} />
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,12 +1,14 @@
 import { createContext, useState, useEffect } from 'react';
 import { products } from "../assets/data.js"
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export const ShopContext = createContext()
 
 const ShopContextProvider = (props) => {
   const currency = 'S'
   const delivery_charges = 10
+  const navigate = useNavigate()
 
   const [search, setSearch] = useState("")
   const [showSearch, setShowSearch] = useState(false)
@@ -50,13 +52,32 @@ const ShopContextProvider = (props) => {
     }
     return totalCount
   }
+  // updating the Quantity 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems)
+    cartData[itemId][size] = quantity
+    setCartItems(cartData)
+  }
 
-  useEffect(() => {
-    console.log(cartItems)
-  }, [cartItems])
+  // Getting total cart amount
+  const getCartAmount = () => {
+    let totalAmount = 0
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items)
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item]
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    return totalAmount
+  }
 
-
-  const contextValue = { products, currency, delivery_charges, search, setSearch, showSearch, setShowSearch, addToCart, getCartCount, cartItems }
+  const contextValue = { products, currency, delivery_charges, search, setSearch, showSearch, setShowSearch, addToCart, getCartCount, cartItems, updateQuantity, getCartAmount, navigate }
 
   return (
     <ShopContext.Provider value={contextValue}>
